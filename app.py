79a81faf64a52c9975feb75bd0d9050a787906e0
Carlos Casals <carlos.casals@zurrose.com>
2022-09-28 16:32:17 +0200

    remove token

--- app.py
+++ app.py
@@ -3,24 +3,24 @@ from flask import Flask, render_template
 # import snowflake.connector
 # import pandas as pd
 # import json
-import jwt
-import time
-import os
+# import jwt
+# import time
+# import os
 
-METABASE_SITE_URL = os.environ['METABASE_SITE_URL']
-METABASE_SECRET_KEY = os.environ['METABASE_SECRET_KEY']
+# METABASE_SITE_URL = os.environ['METABASE_SITE_URL']
+# METABASE_SECRET_KEY = os.environ['METABASE_SECRET_KEY']
 
 
-payload = {
-  "resource": {"question": 3},
-  "params": {
+# payload = {
+#   "resource": {"question": 3},
+#   "params": {
     
-  },
-  "exp": round(time.time()) + (60 * 10) # 10 minute expiration
-}
-token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
+#   },
+#   "exp": round(time.time()) + (60 * 10) # 10 minute expiration
+# }
+# token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
 
-iframeUrl = METABASE_SITE_URL + "/embed/question/" + token.decode('utf-8') + "#bordered=true&titled=false"
+# iframeUrl = METABASE_SITE_URL + "/embed/question/" + token.decode('utf-8') + "#bordered=true&titled=false"
 
 # conn = snowflake.connector.connect(
 #     user = os.getenv('DB_SNOW_USER'),
