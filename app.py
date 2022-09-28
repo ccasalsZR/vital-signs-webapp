3a537b657fc30b154125c4ffc5076b8057711f81
Carlos Casals <carlos.casals@zurrose.com>
2022-09-28 09:30:25 +0200

    test env var

--- app.py
+++ app.py
@@ -22,14 +22,14 @@ token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
 
 iframeUrl = METABASE_SITE_URL + "/embed/question/" + token.decode('utf-8') + "#bordered=true&titled=false"
 
-conn = snowflake.connector.connect(
-    user = os.getenv('DB_SNOW_USER'),
-    password= os.getenv('DB_SNOW_PASS'),
-    account='jt36375.eu-central-1',
-    warehouse='X_SMALL_WH',
-    database='DATAHUB'
-    # role='VITAL_SIGNS'
-)
+# conn = snowflake.connector.connect(
+#     user = os.getenv('DB_SNOW_USER'),
+#     password= os.getenv('DB_SNOW_PASS'),
+#     account='jt36375.eu-central-1',
+#     warehouse='X_SMALL_WH',
+#     database='DATAHUB'
+#     # role='VITAL_SIGNS'
+# )
 
 # Create a Flask Instance
 app = Flask(__name__)
@@ -53,46 +53,48 @@ def index():
     labels = [row[0] for row in data]
     values = [row[1] for row in data]
 
-    # execute SQL statement via cursor
-    cur=conn.cursor()
-    cur.execute('''
-        WITH CASH_FLOW AS (
-                SELECT SUM(INVESTMENT_CF) + SUM(OPERATING_CF) AS FREE_CASH_FLOW 
-                FROM ACCESS_LAYER.VS_VIEW_NWC_CASHFLOW  
-                WHERE 1=1
-                    AND SUBSEGMENT LIKE 'Group%'
-                    AND DATE_TRUNC(YEAR,DATEEXTRACT) = DATE_TRUNC(YEAR,CURRENT_DATE())
-            ) 
-            ,PERFORMANCE_VIEW AS (
-                SELECT SUM(IFF(CODE = 'PV300000', VALUE_CHF, 0)) AS EXTERNAL_REVENUE 
-                    ,SUM(IFF(CODE = 'PV500500', VALUE_CHF, 0)) AS CONTRIBUTION_MARGIN_3
-                FROM ACCESS_LAYER.VS_VIEW_PERFORMANCE_VIEW_2  
-                WHERE 1=1
-                    AND "TYPE" = 'perf_view'
-                --	AND SEGMENT = 'Group'
-                    AND DATE_TRUNC(YEAR,DATEEXTRACT) = DATE_TRUNC(YEAR,CURRENT_DATE())
-            )
-        SELECT * 
-        FROM CASH_FLOW
-        LEFT JOIN PERFORMANCE_VIEW
-    ''')
+    # # execute SQL statement via cursor
+    # cur=conn.cursor()
+    # cur.execute('''
+    #     WITH CASH_FLOW AS (
+    #             SELECT SUM(INVESTMENT_CF) + SUM(OPERATING_CF) AS FREE_CASH_FLOW 
+    #             FROM ACCESS_LAYER.VS_VIEW_NWC_CASHFLOW  
+    #             WHERE 1=1
+    #                 AND SUBSEGMENT LIKE 'Group%'
+    #                 AND DATE_TRUNC(YEAR,DATEEXTRACT) = DATE_TRUNC(YEAR,CURRENT_DATE())
+    #         ) 
+    #         ,PERFORMANCE_VIEW AS (
+    #             SELECT SUM(IFF(CODE = 'PV300000', VALUE_CHF, 0)) AS EXTERNAL_REVENUE 
+    #                 ,SUM(IFF(CODE = 'PV500500', VALUE_CHF, 0)) AS CONTRIBUTION_MARGIN_3
+    #             FROM ACCESS_LAYER.VS_VIEW_PERFORMANCE_VIEW_2  
+    #             WHERE 1=1
+    #                 AND "TYPE" = 'perf_view'
+    #             --	AND SEGMENT = 'Group'
+    #                 AND DATE_TRUNC(YEAR,DATEEXTRACT) = DATE_TRUNC(YEAR,CURRENT_DATE())
+    #         )
+    #     SELECT * 
+    #     FROM CASH_FLOW
+    #     LEFT JOIN PERFORMANCE_VIEW
+    # ''')
 
-    df = pd.DataFrame(cur.fetchall())
+    # df = pd.DataFrame(cur.fetchall())
 
-    free_cash_flow = df.iloc[0][0]
-    external_revenue = df.iloc[0][1]/1000
-    contribution_margin_3 = df.iloc[0][2]/1000
+    # free_cash_flow = df.iloc[0][0]
+    # external_revenue = df.iloc[0][1]/1000
+    # contribution_margin_3 = df.iloc[0][2]/1000
 
     first_name = 'Carlos'
     stuff = 'This is <strong> Bold Text</strong>'
+
+    
     return render_template('index.html',first_name=first_name
         ,stuff=stuff
         ,labels=labels
         ,values=values
-        ,free_cash_flow=free_cash_flow
-        ,external_revenue=external_revenue
-        ,contribution_margin_3=contribution_margin_3
         ,iframeUrl=iframeUrl
+        # ,free_cash_flow=free_cash_flow
+        # ,external_revenue=external_revenue
+        # ,contribution_margin_3=contribution_margin_3
         )
 
 @app.route('/user/<name>')
